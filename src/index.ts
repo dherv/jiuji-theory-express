4f8b036e98215a4a67d3656eb90981004be808a9
dherv <damien.herv@gmail.com>
2020-08-02 22:50:26 +0900

    chore(logs): add Sentry configuration

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,14 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import * as Sentry from '@sentry/node';
 import loggerStream from './config/winston';
 
 dotenv.config();
-
+Sentry.init({
+  dsn:
+    'https://1f810cd5f8774fb1873e49e94bfb1693@o424697.ingest.sentry.io/5374643',
+});
 /**
  * App Variables
  */
@@ -30,10 +34,17 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(morgan('combined', { stream: loggerStream }));
+app.use(Sentry.Handlers.requestHandler());
+
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
 
 /**
  * Server Activation
